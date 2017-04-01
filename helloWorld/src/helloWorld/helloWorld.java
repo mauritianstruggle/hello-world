1c573d002e9bde9f594bd6d58ecb58b95ce3dbc7
mauritianstruggle <mauritianstruggle@gmail.com>
2017-04-01 15:40:44 +0100

    initial commit
    meme

---
+++ helloWorld/src/helloWorld/helloWorld.java
@@ -0,0 +1,20 @@
+package helloWorld;
+
+public class helloWorld
+{
+
+	public static void main(String[] args) 
+	{
+		int a = 3;
+		int b = 6;
+		a = a + 4;
+		a = a + b;
+		String name = "Mauritian";
+		double pi = 3.1415926535879323846;
+		System.out.println("Hello YouTube");
+		System.out.println(name + " " + pi);
+		System.out.println(a);
+
+	}
+
+}
